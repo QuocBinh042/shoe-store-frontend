c491881eaf1f4e77083794bb336b014d892b1e14
QuocBinh042 <tranlequocbinh2003@gmail.com>
2025-05-19 15:41:52 +0700

    feat: gen voucher

--- src/services/voucherService.js
+++ src/services/voucherService.js
@@ -7,3 +7,18 @@ export const fetchVoucherById = async (id) => {
     const data = await fetchData(`voucher/by-voucher-id/${id}`);
     return data.data;
 };
+
+export const getAllVouchers = async () => {
+    const data = await fetchData('voucher');
+    return data;
+};
+
+export const createBatchVouchers = async (voucherList) => {
+    const data = await postData('voucher/create-batch', voucherList);
+    return data;
+};
+
+export const deleteVoucher = async (id) => {
+    const data = await deleteData(`voucher/delete/${id}`);
+    return data;
+};
